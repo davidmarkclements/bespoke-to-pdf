45e575eb8046200f82488c82873af56bbda8b3bc
davidmarkclements <huperekchuno@googlemail.com>
2015-02-23 23:43:35 +0000

    resolve --app correctly

--- cmd.js
+++ cmd.js
@@ -3,6 +3,8 @@ var d2p = require('./index.js');
 var argv = require('minimist')(process.argv.slice(2));
 var url = argv._.filter(isNaN)[0];
 var slides = +~~(argv._.filter(isFinite)[0]);
+var path = require('path');
+
 argv.slide = Object.keys(argv).map(function(k) {
   var m = k.match(/slide-([a-zA-Z]+)/)
   return m && m[1];
@@ -12,5 +14,10 @@ argv.slide = Object.keys(argv).map(function(k) {
   try { o[k] = JSON.parse(o[k]); } catch (e) { }
   return o;
 }, {});
+
+
+if (argv.app) {
+  argv.app = path.resolve(process.cwd(), argv.app)
+}
 
 d2p(url, slides, argv).pipe(process.stdout);
